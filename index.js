4d5214836804d3a54dbd9d29c9c7871576352363
NightCandle <instkffff@gmail.com>
2019-01-15 00:30:27 +0800

    .

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 const Telegraf = require('telegraf')
 require('dotenv').config({path:'conf.env'})
-//const Query = require('jsonpath')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-async function GetPony (query = '' , offset, limit){
-	const pony = require('./pony.json')[query]
-	return pony
+const pony = require('./pony.json')
 
-  console.log(pony)
-}
+bot.on('inline_query', (ctx) => {
+  //console.log(ctx)
+  //console.log(ctx.update.inline_query.query)
+  const PonyJson = []
+  for ( i = 0; i < ctx.update.inline_query.query; i++ ){
+    var ponys = pony[i]
+    PonyJson.push({
+      type: 'photo',
+      id: ponys.slug,
+      photo_url: `http://155.94.228.151:3000/${ponys.name}`,
+      thumb_url: `http://155.94.228.151:3000/${ponys.name}`
+    })  
+  } 
 
+  console.log(PonyJson)
 
+  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, PonyJson)
 
-bot.on('inline_query',async({inlineQuery, answerInlineQuery}) => {
-	const offset = parseInt(inlineQuery.offset) || 0
-	const Ponys = await GetPony(inlineQuery.query, offset, 3)
-	const results = {
-		type: 'photo',
-		id: Pony.slug,
-		photo_url: `http://155.94.228.151:3000/${Pony.name}`,
-		thumb_url: `http://155.94.228.151:3000/${Pony.name}`,
-		title: Pony.name,
-	}
-
-	return answerInlineQuery(results, {next_offset: offset + 3})
-})
-
-const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/zeit/serve-handler#options
-  return handler(request, response);
-})
-
-bot.catch((err) => {
-  console.log('Error', err)
 })
 
 
