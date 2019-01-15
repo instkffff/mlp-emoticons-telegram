8add479d0bdcd5c632eb28e0541ca05f1a3e88df
NightCandle <instkffff@gmail.com>
2019-01-15 17:11:51 +0800

    .

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ bot.on('inline_query', async({ inlineQuery, answerInlineQuery}) => {
     results.push({
       type: 'photo',
       id: ponys.slug,
-      thumb_url: `http://155.94.228.151:3000/${ponys.name}`,
-      photo_url: `http://155.94.228.151:3000/${ponys.name}`
+      thumb_url: 'https://m.media-amazon.com/images/M/MV5BNjk4MzkyOTQwNF5BMl5BanBnXkFtZTgwODE5MTk1MzE@._V1_SX300.jpg',
+      photo_url: 'https://m.media-amazon.com/images/M/MV5BNjk4MzkyOTQwNF5BMl5BanBnXkFtZTgwODE5MTk1MzE@._V1_SX300.jpg'
+      //thumb_url: `http://155.94.228.151:3000/${ponys.name}`,
+      //photo_url: `http://155.94.228.151:3000/${ponys.name}`
     })  
   } 
 
