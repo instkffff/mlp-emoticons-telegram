37b3ae078df04de13650418bc54eeae3e7e3ac08
NightCandle <instkffff@gmail.com>
2019-01-15 16:00:07 +0800

    .

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const pony = require('./pony.json')
 bot.on('inline_query', (ctx) => {
   //console.log(ctx)
   //console.log(ctx.update.inline_query.query)
-  const PonyJson = []
+  const results = []
   for ( i = 0; i < ctx.update.inline_query.query; i++ ){
     var ponys = pony[i]
-    PonyJson.push({
+    results.push({
       type: 'photo',
       id: ponys.slug,
-      photo_url: `http://155.94.228.151:3000/${ponys.name}`,
-      thumb_url: `http://155.94.228.151:3000/${ponys.name}`
+      thumb_url: `http://155.94.228.151:3000/${ponys.name}`,
+      photo_url: `http://155.94.228.151:3000/${ponys.name}`
     })  
   } 
 
-  console.log(PonyJson)
+  console.log(results)
 
-  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, PonyJson)
+  return ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, results)
 
 })
 
